e8515efc1963f07b2111e4f77bda5f92ad8f34e8
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-11-13 10:58:53 +0700

    📦 scaffold dashboard

--- src/pages/dashboard.svelte
+++ src/pages/dashboard.svelte
@@ -1,1 +1,18 @@
-<h1>Dashboard</h1>
+<script lang="ts">
+    import {LoginCredentials} from "../store/LoginCredentials.svelte";
+    console.log(LoginCredentials);
+</script>
+
+<div class="relative flex flex-col w-screen h-screen">
+    <div class="flex gap-2 justify-between px-4 py-2">
+        <div class="text-3xl font-bold">Lagra</div>
+        <div>
+            <span>{LoginCredentials.user.username}</span>
+        </div>
+    </div>
+    <div class="relative flex flex-1 h-full">
+        <div class="md:w-[20%]">Sidebar</div>
+
+        <div class="w-full">Content</div>
+    </div>
+</div>
